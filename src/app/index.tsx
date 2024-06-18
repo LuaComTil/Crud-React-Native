56949b8cf2cb67ebe37d9a1b3de2d4ad58882cad
LuaComTil <luadefreitasramos@gmail.com>
2024-06-18 16:29:15 -0300

    added register products id at db

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,36 @@
-import { Input } from "@/components/Input"
 import { useState } from "react"
-import { Button, View } from "react-native"
+import { Alert, Button, View } from "react-native"
+
+import { Input } from "@/components/Input"
+import { useProductDatabase } from "../database/useProductDatabase"
 
 export default function Index() {
-    const [] = useState("")
-    const [] = useState("")
-    const [] = useState("")
+    const [id, setId] = useState("")
+    const [name, setName] = useState("")
+    const [quantity, setQuantity] = useState("")
+    const [products, setProducts] = useState([])
 
-    return <View  style={{flex: 1, justifyContent:"center", padding: 32, gap: 16}}>
-        <Input placeholder="Name"></Input>
-        <Input placeholder="Amount"></Input>
-        <Button title="Save"/>
+    const productDatabase = useProductDatabase()
+
+    async function create() {
+        try {
+            if(isNaN(Number(quantity))) {
+                return Alert.alert("Quantity", "Quantity needs to be a number")
+            }
+            const response = await productDatabase.create({ name, quantity: Number(quantity)})
+            Alert.alert("Produto cadastrado com ID: " + response.insertedRowId)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+    
+    <View  style={{flex: 1, justifyContent:"center", padding: 32, gap: 16}}>
+        <Input placeholder="Name" onChangeText={setName} value={name}></Input>
+        <Input placeholder="Quantity" onChangeText={setQuantity} value={quantity}></Input>
+        <Button title="Save" onPress={create}/>
     </View>
 
+    )
 }
